3ff09236f1d6c3ca10ba9c669f66bf0c97db6025
Rico Simlinger <r.simlinger@secupay.ag>
2018-08-03 09:41:34 +0200

    test1

--- app/Secucard.Connect.DemoApp/Program.cs
+++ app/Secucard.Connect.DemoApp/Program.cs
@@ -5,7 +5,7 @@
         private static void Main()
         {
             // SMART examples
-            _01_smart_transaction.simple.Run();
+            //_01_smart_transaction.simple.Run();
 
             // Payment examples
             _02_client_payments.Run_Sample.runAllSamples();
